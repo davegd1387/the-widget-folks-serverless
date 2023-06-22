330a32030994e4d92de2808a65541588b0952439
“davegd1387” <davegd1387@gmailcom>
2023-06-22 19:04:56 -0400

    console.log cookies

--- functions/corsObj.js
+++ functions/corsObj.js
@@ -1,5 +1,9 @@
 const corsObj = {
-  origin: ["https://the-widget-folks.netlify.app"],
+  origin: [
+    "https://the-widget-folks.netlify.app",
+    "http://localhost:3000",
+    "http://localhost:39447",
+  ],
   methods: ["POST", "PUT", "GET", "OPTIONS", "HEAD"],
   credentials: true,
 };
